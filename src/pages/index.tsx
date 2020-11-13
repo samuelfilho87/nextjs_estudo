99d0d5cdfee12d7dfc0a8d8265025aaa8afa7341
Samuel Damasceno Filho <samuelfilho@outlook.com>
2020-11-13 01:17:33 -0300

    Utilizando import dinamico

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,6 +26,12 @@ export default function Home({ recommendedProducts }: HomeProps) {
   //   })
   // }, []);
 
+  async function handleSum() {
+    const math = (await import('../lib/math')).default;
+
+    alert(math.sum(3, 5));
+  }
+
   return (
     <div>
       <section>
@@ -41,6 +47,8 @@ export default function Home({ recommendedProducts }: HomeProps) {
           })}
         </ul>
       </section>
+
+      <button onClick={handleSum}>Sum!</button>
     </div>
   )
 }
