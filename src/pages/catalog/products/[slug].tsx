bb49780a85118c2e8e3a135c9ab7873f8ebf0b34
Samuel Damasceno Filho <samuelfilho@outlook.com>
2020-11-13 01:31:34 -0300

    Lazy load de componentes

---
+++ src/pages/catalog/products/[slug].tsx
@@ -0,0 +1,27 @@
+import {useRouter} from 'next/router';
+import { useState } from 'react';
+import dynamic from 'next/dynamic';
+
+const AddToCartModal = dynamic(
+  () => import('../../../components/addToCartModal'),
+  { loading: () => <p>Loading...</p>, ssr: false }
+)
+
+export default function Product() {
+  const router = useRouter();
+  const [isAddToCartModalVisible, setIsAddToCartModalVisible] = useState(false);
+
+  function handleAddToCart() {
+    setIsAddToCartModalVisible(true);
+  }
+
+  return (
+    <div>
+      <h1>{router.query.slug}</h1>
+
+      <button onClick={handleAddToCart}>Add to cart</button>
+
+      { isAddToCartModalVisible && <AddToCartModal /> }
+    </div>
+  )
+}
